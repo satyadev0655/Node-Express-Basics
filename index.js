136302f571240a9c80d339dd0a222357b35c5acb
satyadev0655 <satya.dev0655@gmail.com>
2022-04-26 14:39:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3010;
-const path = require('path');
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
+  console.log(req)
   res.send('Hello World!');
-  //res.sendFile(path.resolve('pages/index.html'));
 });
 
 app.listen(port, () => {
